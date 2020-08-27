a08de95d208592d7887a3528e3570c85b4a7ef8f
Alex Lerner <alernerdev@gmail.com>
2020-08-27 18:58:45 -0400

    created travis.yml file and encrypted service-account file

---
+++ encryptServiceFile.sh
@@ -0,0 +1,3 @@
+# service-account.json was created by google cloud
+# alernerdev/docker-complex-k8s is the case sensitive github repository name
+travis encrypt-file service-account.json -r alernerdev/docker-complex-k8s
